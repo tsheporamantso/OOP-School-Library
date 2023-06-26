e64bdcd4c7c57517c181499ec679f7ea2fb5f676
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-26 16:02:14 +0200

    Add person class

---
+++ person.rb
@@ -0,0 +1,25 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name: 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    return true if is_of_age? || @parent_permission
+
+    false
+  end
+
+  private
+
+  def of_age?
+    return true if age >= 18
+
+    false
+  end
+end
