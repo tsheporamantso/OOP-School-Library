87cf3dfae6b66db4e616888331b296237d744e78
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-27 21:14:47 +0200

    Add test code

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 require './nameable'
+require './decorator'
+require './capitalize_decorator'
+require './trimmed_decorator'
 
 class Person < Nameable
   attr_reader :id
@@ -30,3 +33,14 @@ class Person < Nameable
     false
   end
 end
+
+person = Person.new(22, name: 'maximilianus')
+person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+
+puts capitalized_trimmed_person.correct_name
