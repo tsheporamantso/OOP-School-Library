3e3004b0afaf71270d23d5f861ff9c079bc21193
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-28 16:41:32 +0200

    Add belong to association between rental and person

--- rental.rb
+++ rental.rb
@@ -1,11 +1,14 @@
 require './book'
+require './person'
 
 class Rental
   attr_accessor :date
 
-  def initialize
+  def initialize(date, book, person)
     @date = date
     @book = book
-    book.rental << self
+    book.rentals << self
+    @person = person
+    person.rentals << self
   end
 end
