2d1e366cfd99266c2fe42b71ea614367e949ed25
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-28 13:11:59 +0200

    Add classroom class with add_student method

---
+++ classroom.rb
@@ -0,0 +1,16 @@
+require './student'
+
+class Classroom
+  attr_accessor :label
+  attr_reader :student
+
+  def initialize(label)
+    @label = label
+    @student = []
+  end
+
+  def add_student(student)
+    @student.push(student)
+    student.classroom = self
+  end
+end
