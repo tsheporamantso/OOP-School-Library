6b6e179a1f666ab1a0865ec49b0398d9572781b6
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-28 13:16:33 +0200

    Add associate relationship between student and classroom

--- student.rb
+++ student.rb
@@ -1,6 +1,9 @@
 require './person'
+require './classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name: 'unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
@@ -8,5 +11,9 @@ class Student < Person
 
   def play_hook
     '¯\(ツ)/¯'
+  end
+
+  def add_classroom(classroom)
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
